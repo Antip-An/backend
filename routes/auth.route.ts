31d8555b62b79be98d2aa6fd27202fd1b73e0b58
Antip-An <anechka-antipina@mail.ru>
2022-03-15 01:18:30 +0300

    -

--- routes/auth.route.ts
+++ routes/auth.route.ts
@@ -10,13 +10,12 @@ const roleMiddleware = require("../middleware/role.middleware")
 
 router.post('/api/singup', [
    check("login", "Имя пользователя должно быть введено").notEmpty(),
-   check("password", "Пароль должен быть большей 4-х символов").isLength(4),
+   check("password", "Пароль должен быть больше 4-х символов").isLength(4),
 ], authController.singup)
 
 router.post('/api/login', authController.login)
 
 router.get('/getusers', roleMiddleware(["Admin"]), authController.getUsers)
-
 
 
 
